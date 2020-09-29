c9b1175e49bd7a334fcee520e95147d221baa0fa
Dominik Holland <dominik.holland@qt.io>
2020-09-29 22:15:18 +0200

    First basic list handling for favorites
    * Created a parseFunction for the LMS extended format
    * Add support for more coverart URL types:
      Tune-In covers
      Spotify covers
      Local media covers
      spotify playlists and some radio covers still not working

--- bin/service/music-server/lms/music-list.js
+++ bin/service/music-server/lms/music-list.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const LMSClient = require('./lms-client');
+
 module.exports = class List {
-  constructor(musicServer, url) {
+  constructor(musicServer, url, zone_id) {
     this._musicServer = musicServer;
     this._url = url;
+    this._zone_id = zone_id;
+    if (url.endsWith("favorites"))
+        this._client = new LMSClient(this._zone_id);
 
     this._total = Infinity;
     this._items = [];
@@ -17,61 +22,61 @@ module.exports = class List {
   }
 
   async get(start, length) {
-    const items = this._items;
-    const end = start + (length || 1);
+//    const items = this._items;
+//    const end = start + (length || 1);
 
-    while (items.length < this._total && items.length < end) {
-      let chunk = {items: [], total: 0};
+//    while (items.length < this._total && items.length < end) {
+//      let chunk = {items: [], total: 0};
 
-      try {
-        chunk = await this._call('GET', this._url + '/' + items.length);
-      } catch (err) {
-        console.error('[ERR!] Could not fetch list fragment: ' + err.message);
-      }
+//      try {
+//        chunk = await this._call('GET', this._url + '/' + items.length);
+//      } catch (err) {
+//        console.error('[ERR!] Could not fetch list fragment: ' + err.message);
+//      }
 
-      this._items.splice(items.length, Infinity, ...chunk.items);
-      this._total = chunk.total;
+//      this._items.splice(items.length, Infinity, ...chunk.items);
+//      this._total = chunk.total;
+//    }
+
+    let items = [];
+    if (this._client) {
+        let response = await this._client.command('favorites items ' + start + " " + length + " want_url%3A1");
+        let obj = this._client.parseAdvancedQueryResponse(response, 'id', ['title']);
+        this._total = obj.count;
+        items = obj.items;
     }
+
+    let processed = []
+    for (var key in items) {
+      processed.push({
+                    id: items[key].id,
+                    title: items[key].name,
+                    image: await this._client.artworkFromUrl(items[key].url)
+                })
+    }
+
+    //Replace the correct items ?
+    //How to emit changes in the list or a complete reset ?
 
     return {
       total: this._total,
-      items: items.slice(start, end),
+      items: processed,
     };
   }
 
   async insert(position, ...items) {
-    try {
-      await this._call('POST', this._url + '/' + position, items);
-    } catch (err) {
-      console.error('[ERR!] Could not insert list fragment: ' + err.message);
-    }
-
-    this.reset();
+    await this._client.command('favorites add item_id%3A' + position + 'title%3A' + items.title + ' url%3A' + items.id);
   }
 
   async replace(position, ...items) {
-    try {
-      await this._call('PUT', this._url + '/' + position, items);
-    } catch (err) {
-      console.error('[ERR!] Could not replace list fragment: ' + err.message);
-    }
-
-    this.reset();
+     await this.delete(position, 1)
+     await this.insert(position, ...items)
   }
 
   async delete(position, length) {
-    try {
-      await this._call('DELETE', this._url + '/' + position + '/' + length);
-    } catch (err) {
-      console.error('[ERR!] Could not delete list fragment: ' + err.message);
+    for (var i=0; i<length; i++) {
+        // TODO Test whether we need to really increase the position
+        await this._client.command('favorites delete item_id%3A' + position + i);
     }
-
-    this.reset();
-  }
-
-  _call() {
-    const callback = () => this._musicServer.call(...arguments);
-
-    return (this._last = this._last.then(callback, callback));
   }
 };
