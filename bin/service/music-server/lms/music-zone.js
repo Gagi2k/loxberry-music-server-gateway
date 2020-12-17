14676ae27eb5d164c05f25361e29f5e8f9d46b67
Dominik Holland <dominik.holland@googlemail.com>
2020-12-17 20:37:03 +0100

    Add sayFavVolumeBoost config option
    This value is added to the current volume when starting the
    sayFav script.

--- bin/service/music-server/lms/music-zone.js
+++ bin/service/music-server/lms/music-zone.js
@@ -286,7 +286,7 @@ module.exports = class MusicZone {
   async playRoomFav(id, favoriteId, fav_name) {
     if (config.sayFav) {
         let fav_id = favoriteId % 1000000;
-        await this._client.execute_script("sayFav", { zones: this._id, fav_id, fav_name: escape(fav_name), volume: this._player.volume})
+        await this._client.execute_script("sayFav", { zones: this._id, fav_id, fav_name: escape(fav_name), volume: this._player.volume + config.sayFavVolumeBoost})
     }
 
     await this.play(id, favoriteId);
